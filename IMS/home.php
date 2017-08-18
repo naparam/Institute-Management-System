bffe1b9f2dcfaf2082229f800f449d4ef1197095
Napa Ram <desainarpat1994@gmail.com>
2017-08-18 21:41:48 +0530

    Add files via upload

---
+++ IMS/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="css/home.css"/>
+<title>::. Build Bright University .::</title>
+</head>
+
+<body>
+<div id="content">
+	<div >
+    	<img src="picture/master.jpg" width="805" height="308"  />
+    </div>
+    
+    <div id="space"></div>
+    
+    <div id="question">
+    	What do you know about Institute Management System?
+    </div>
+    
+    <div id="space"></div>
+    
+    <div style="text-indent:40px; text-align:justify">
+    To overcome the complexity of this environment and to manage its resources with optimal efficiency, Institute management need to have robust and integrated Institute Management System for their day to day operation. ?Institute Management System? is unparallel and comprehensive Institute Software that covers each and every entity of Institute like students, faculties and management.
+
+  </div>
+</div><!--end of content -->
+</body>
+</html>
